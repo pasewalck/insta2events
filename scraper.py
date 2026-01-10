59fa2b613a568f9f7304806766f6ebbeed80090c
pasewalck <pasewalck@posteo.net>
2026-01-10 14:24:23 +0100

    Improve sync tracker: Track more data and cleaner setup. Start to improve scraper code (Some parts are a bit messy now. These should be cleaned up!).

--- scraper.py
+++ scraper.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from itertools import dropwhile, takewhile
 
 import instaloader
-from instaloader import Instaloader
+from instaloader import Instaloader, Hashtag, Profile, QueryReturnedNotFoundException
 
 from config import DATA_PARENT_FOLDER, LOGIN_SESSION_FILE, LOGIN_USERNAME, SCRAPE_ACCOUNTS, SCRAPE_HASHTAGS, \
-    POSTS_FOLDER_NAME
-from tracker import SocialMediaTracker, PostTracker, use_tracker
+    POSTS_FOLDER_NAME, SYNC_SINCE
+from tracker import SocialMediaTracker, PostTracker, use_tracker, AccountDetails
 
 account_usernames = SCRAPE_ACCOUNTS.split(",") if SCRAPE_ACCOUNTS != "" else []
 hashtags = SCRAPE_HASHTAGS.split(",") if SCRAPE_ACCOUNTS != "" else []
@@ -14,14 +14,36 @@ hashtags = SCRAPE_HASHTAGS.split(",") if SCRAPE_ACCOUNTS != "" else []
 until = datetime.now()
 
 
-def download(l: Instaloader, posts, sync_tracker: SocialMediaTracker):
-    posts = takewhile(lambda p: p.date > sync_tracker.sync_state, dropwhile(lambda p: p.date > until, posts))
+def download(l: Instaloader, mode, target, sync_tracker: SocialMediaTracker):
+    try:
+        obj = instaloader.Profile.from_username(l.context,
+                                                target) if mode == Profile else instaloader.Hashtag.from_name(
+            l.context, target)
+    except QueryReturnedNotFoundException:
+        return
+
+    identifier = obj.userid if mode is Profile else (f"#{obj.name}" if mode is Hashtag else None)
+    posts = obj.get_posts() if mode is Profile else obj.get_posts_resumable()
+    sync_state = sync_tracker.sync_states[
+        identifier] if sync_tracker.sync_states.get(identifier) is not None else datetime.fromisoformat(
+        SYNC_SINCE)
+    print(f"Scraping for {identifier} since latest sync state: {sync_state.strftime('%Y-%m-%d %H:%M:%S')}")
+    posts = takewhile(lambda p: p.date > sync_state, dropwhile(lambda p: p.date > until, posts))
     i_tracker = 0
     for post in posts:
         l.download_post(post, post.mediaid)
-        sync_tracker.posts.append(PostTracker(post.mediaid, post.date, True))
+        if sync_tracker.posts.get(post.mediaid) is not None:
+            sync_tracker.posts[post.mediaid].sources.append(identifier)
+        else:
+            owner_profile = post.owner_profile
+            account_details = AccountDetails(owner_profile.username, owner_profile.userid, owner_profile.biography,
+                                             owner_profile.external_url)
+            print(owner_profile.external_url)
+            sync_tracker.posts[post.mediaid] = (PostTracker(post.mediaid, post.date, True, identifier, account_details))
         i_tracker += 1
-    return i_tracker
+        print(f"{i_tracker}/?")
+    sync_tracker.sync_states[identifier] = until
+    print(f"Scraped {i_tracker} for {identifier}")
 
 
 def main():
@@ -35,19 +57,12 @@ def main():
 
     with use_tracker() as sync_tracker:
 
-        print(f"Scraping everything since latest sync state: {sync_tracker.sync_state.strftime('%Y-%m-%d %H:%M:%S')}")
+        print(f"Scraping everything since latest sync states!")
 
         for hashtag in hashtags:
-            print(f"Scraping for #{hashtag}")
-            instaloader_hashtag = instaloader.Hashtag.from_name(l.context, hashtag)
-            count = download(l, instaloader_hashtag.get_posts_resumable(), sync_tracker)
-            print(f"Scraped {count} for #{hashtag}")
-
+            download(l, instaloader.Hashtag, hashtag, sync_tracker)
         for account_username in account_usernames:
-            print(f"Scraping from @{account_username}")
-            instaloader_profile = instaloader.Profile.from_username(l.context, account_username)
-            count = download(l, instaloader_profile.get_posts(), sync_tracker)
-            print(f"Scraped {count} for #{account_username}")
+            download(l, instaloader.Profile, account_username, sync_tracker)
 
         print(f"All new content was scraped. Updating sync state to: {until.strftime('%Y-%m-%d %H:%M:%S')}")
 
