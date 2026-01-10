8557690216041140e497829bcebd5d323cc87da2
jana <jana.pasewalck@posteo.net>
2026-01-10 14:25:15 +0100

    Use new data from sync tracker on interpreter, add basic classifier mode and improve flow.

--- interpreter.py
+++ interpreter.py
@@ -1,17 +1,60 @@
 import datetime
 import os
+import re
 
+from numpy.f2py.auxfuncs import throw_error
 from ollama import chat
 
-from config import MODEL_LARGE, MODEL_SMALL, PROMPT_OCR_PARSE_FILE, PROMPT_VALUE_FILE, DATA_PARENT_FOLDER, LLM_USE_OCR, \
-    PROMPT_PARSE_FILE, POSTS_FOLDER_NAME, OCR_OUTPUT_FILE_NAME, LLM_OUTPUT_FILE_NAME
+from config import MODEL_LARGE, MODEL_SMALL, PROMPT_OCR_PARSE_FILE, PROMPT_VALUE_FILE, DATA_PARENT_FOLDER, \
+    INTERPRETER_USE_OCR, \
+    PROMPT_PARSE_FILE, POSTS_FOLDER_NAME, OCR_OUTPUT_FILE_NAME, LLM_OUTPUT_FILE_NAME, INTERPRETER_CLASSIFIER_MODE, \
+    RERUN_INTERPRETER, IMPROVE_OCR_WITH_LLM
 from tracker import use_tracker, PostTracker
-from util.files_operations import load_file, load_json, write_json
+from util.files_operations import load_file, write_json
 
 # Load prompts
 prompt_ocr_parse = load_file(PROMPT_OCR_PARSE_FILE)
 prompt_value = load_file(PROMPT_VALUE_FILE)
 prompt_parse = load_file(PROMPT_PARSE_FILE)
+
+# Lists of keywords in English and German
+english_keywords = [
+    'event', 'festival', 'concert', 'party', 'celebration',
+    'gathering', 'exhibition', 'launch', 'ceremony', 'workshop',
+    'conference', 'meetup', 'gala', 'opening', 'show'
+]
+german_keywords = [
+    'ereignis', 'festival', 'konzert', 'party', 'feier',
+    'zusammenkunft', 'ausstellung', 'einführung', 'zeremonie', 'workshop',
+    'konferenz', 'treffen', 'gala', 'eröffnung', 'show', 'aktion'
+]
+english_weekdays = [
+    'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
+]
+german_weekdays = [
+    'montag', 'dienstag', 'mittwoch', 'donnerstag', 'freitag', 'samstag', 'sonntag'
+]
+
+english_months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october",
+                  "november", "december"]
+german_months = ["januar", "februar", "märz", "april", "mai", "juni", "juli", "august", "september", "oktober",
+                 "november", "dezember"]
+
+# Dates Pattern (format: DD.MM.YYYY or other similar formats)
+date_pattern = r'\d{1,2}[.\-\/][ ]\d{1,2}[.\-\/]'
+
+
+def match_text(text):
+    contains = False
+    for word in (
+            english_keywords + german_keywords + english_weekdays + german_weekdays + german_months + english_months):
+        if word.lower() in text.lower():
+            contains = True
+    match = re.search(date_pattern, text, re.IGNORECASE)
+    if (match is not None and match.group() != "") or contains:
+        return True
+    else:
+        return False
 
 
 def ask(message, large_model):
@@ -51,29 +94,39 @@ def main():
 def run_interpreter(post: PostTracker):
     directory = os.path.join(DATA_PARENT_FOLDER, POSTS_FOLDER_NAME, f"{post.media_id}")
 
-    content_caption = load_file(os.path.join(directory, "data.txt"))
-    content_ocr_raw = load_json(os.path.join(directory, OCR_OUTPUT_FILE_NAME), None)
+    content_caption = load_file(os.path.join(directory, "data.txt"), None)
+    content_ocr_raw = load_file(os.path.join(directory, OCR_OUTPUT_FILE_NAME), "None")
+    use_ocr = INTERPRETER_USE_OCR and content_ocr_raw is not None
 
-    if LLM_USE_OCR:
-        content_ocr_parsed = ask(prompt_ocr_parse.replace("{input}", content_ocr_raw), False)
-        if content_ocr_parsed:
-            content = f"{content_caption}\n\n{content_ocr_parsed}"
-        else:
-            content = content_caption
-    else:
-        content = content_caption
+    seems_to_be_event = classifier(content_caption) or (use_ocr and classifier(content_ocr_raw))
 
-    evaluation = evaluate_content(prompt_value.replace("{input}", content))
+    print(f"Evaluation for {post.media_id}: {seems_to_be_event}")
 
-    if evaluation == "true":
-        result = parse_content(prompt_parse.replace("{input}", content))
+    if seems_to_be_event:
+        input_ocr = (ask(prompt_ocr_parse.replace("{input}", content_ocr_raw),
+                         False) if IMPROVE_OCR_WITH_LLM else content_ocr_raw) if use_ocr else "None"
+        result = parse_content(
+            prompt_parse.replace("{input}", content_caption).replace("{input_ocr}", input_ocr).replace(
+                "{owner_link}", post.account_details.link).replace(
+                "{owner_name}", post.account_details.name).replace("{owner_bio}", post.account_details.bio))
+        print(f"Parsed: {result}" if result != [] and result != "[]" else f"Parse failed!")
     else:
         result = []
 
     write_json(os.path.join(directory, LLM_OUTPUT_FILE_NAME), result)
 
-    print(
-        f"Result for {post.media_id}: Evaluation: {evaluation} and Parse: {'Failed' if result is not [] else 'Success'} and Result: {result}")
+
+def classifier(content):
+    if INTERPRETER_CLASSIFIER_MODE == "basic":
+        return match_text(content)
+    elif INTERPRETER_CLASSIFIER_MODE == "llm":
+        return ai_classifier(content)
+    else:
+        throw_error("No Interpreter Mode")
+
+
+def ai_classifier(content):
+    return evaluate_content(prompt_value.replace("{input}", content)) == "true"
 
 
 if __name__ == "__main__":
