dc2efd432a133c1d2a18027ff9d71a619ca74580
pasewalck <pasewalck@posteo.net>
2026-01-10 15:08:19 +0100

    Moved config.py and llm.py to util.

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,12 @@
 import os
 
-from config import DATA_PARENT_FOLDER, \
+from tracker import use_tracker, PostTracker
+from util.config import DATA_PARENT_FOLDER, \
     INTERPRETER_USE_OCR, \
     PROMPT_PARSE_FILE, POSTS_FOLDER_NAME, OCR_OUTPUT_FILE_NAME, LLM_OUTPUT_FILE_NAME, RERUN_INTERPRETER, \
     OCR_OUTPUT_PARSED_FILE_NAME
-from llm import ask, load_ai_prompt
-from tracker import use_tracker, PostTracker
 from util.files_operations import load_file, write_json
+from util.llm import ask, load_ai_prompt
 
 # Load prompts
 prompt_parse = load_ai_prompt(PROMPT_PARSE_FILE)
@@ -33,7 +33,7 @@ def run_interpreter(post: PostTracker):
     ocr_data = load_file(os.path.join(directory, OCR_OUTPUT_PARSED_FILE_NAME), "None") \
         if post.ocr_improved \
         else load_file(os.path.join(directory, OCR_OUTPUT_FILE_NAME), "None")
-    
+
     use_ocr = INTERPRETER_USE_OCR and ocr_data is not None
 
     result = parse_content(
