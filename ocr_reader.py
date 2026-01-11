779229f9c156babefdb6759941492539052bf5b1
jana <jana.pasewalck@posteo.net>
2026-01-11 19:43:24 +0100

    Add ocr debug image generation and replace group_adjacent_text with a "simpler" approach.

--- ocr_reader.py
+++ ocr_reader.py
@@ -2,6 +2,8 @@ import os
 
 import cv2
 import easyocr
+import numpy as np
+import supervision as sv
 
 from tracker import use_tracker, PostTracker
 from util.config import OCR_OUTPUT_FILE_NAME, OCR_ONLY_ON_AS_EVENT_CLASSIFIED
@@ -9,67 +11,62 @@ from util.files_operations import write_json
 
 
 def run_ocr(post: PostTracker, reader: easyocr.Reader):
-    output_raw = []
-    output_cleaned = []
-    for image_path in post.get_image_paths():
+    output_list = []
+    for i, image_path in enumerate(post.get_image_paths(), start=1):
         try:
             image = cv2.imread(image_path)
             result = reader.readtext(image)
-            cleaned_result = group_adjacent_text(result)
-            output_raw.append(result)
-            output_cleaned.append(cleaned_result)
+            output_list.append(simplify(result))
+            if result is not []:
+                save_debug_image(result, image, f"{post.media_id}-{i}")
         except FileNotFoundError:
             print("Error: The image file was not found.")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
 
-    print(f"For {post.media_id} OCR Results: \n Cleaned: {output_cleaned}")
-    write_json(os.path.join(post.directory(), OCR_OUTPUT_FILE_NAME), output_cleaned)
+    print(f"For {post.media_id} OCR Results: {output_list}")
+    write_json(os.path.join(post.directory(), OCR_OUTPUT_FILE_NAME), output_list)
 
 
-def group_adjacent_text(results, threshold_x=10, threshold_y=10, filter_confidence=0.5):
-    linked_text = []
-    current_group = []
-    current_group_bbox = []
+def save_debug_image(ocr_result, image, file_name):
+    # Code from https://blog.roboflow.com/how-to-use-easyocr/
+    xyxy, confidences, class_ids, label = [], [], [], []
 
-    for (bbox, text, confidence) in results:
-        x_start, y_start = bbox[0]
-        x_end, y_end = bbox[2]
-        y_size = y_end - y_start
+    for detection in ocr_result:
+        bbox, text, confidence = detection[0], detection[1], detection[2]
+        length = len(text)
 
-    for (bbox, text, confidence) in results:
+        x_min = int(min([point[0] for point in bbox]))
+        y_min = int(min([point[1] for point in bbox]))
+        x_max = int(max([point[0] for point in bbox]))
+        y_max = int(max([point[1] for point in bbox]))
 
-        if filter_confidence > confidence:
-            continue
+        xyxy.append([x_min, y_min, x_max, y_max])
+        label.append(f"{text} Conf.: {confidence}")
+        confidences.append(confidence)
+        class_ids.append(0)
 
-        if len(current_group) == 0:
-            current_group.append(text)
-            current_group_bbox.append(bbox)
-            continue
+    detections = sv.Detections(
+        xyxy=np.array(xyxy),
+        confidence=np.array(confidences),
+        class_id=np.array(class_ids)
+    )
 
-        x_start, y_start = bbox[0]
-        x_end, y_end = bbox[2]
-        y_size = y_end - y_start
+    box_annotator = sv.BoxAnnotator()
+    label_annotator = sv.LabelAnnotator()
 
-        last_bbox = current_group_bbox[len(current_group) - 1]
-        last_x_start = last_bbox[0][0]
-        last_x_end = last_bbox[2][0]
-        last_y_start = last_bbox[0][1]
-        last_y_end = last_bbox[2][1]
-        last_y_size = last_y_end - last_y_start
+    annotated_image = box_annotator.annotate(scene=image, detections=detections)
+    annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=label)
+    cv2.imwrite(os.path.join("./ocr", f"{file_name}.jpg"), annotated_image)
 
-        if last_x_end + threshold_x > x_start or last_y_end + threshold_y > y_start:
-            current_group.append(text)
-            current_group_bbox.append(bbox)
-        else:
-            linked_text.append(' '.join(current_group))
-            current_group = [text]
-            current_group_bbox = [bbox]
 
-    if current_group:
-        linked_text.append(' '.join(current_group))
+def simplify(raw_result):
+    result = []
 
-    return linked_text
+    for (bbox, text, confidence) in raw_result:
+        result.append(text)
+
+    return result
 
 
 def main():
