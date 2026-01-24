740cd9f2cd654dd97e468a573db1526e74a32324
jana <jana.pasewalck@posteo.net>
2026-01-24 16:28:27 +0100

    Improve basic classifier by adding "negativ keywords" that indicate an event to be in the past.

--- util/basic_classifier.py
+++ util/basic_classifier.py
@@ -1,15 +1,23 @@
 import re
 
+from tracker import PostTracker
+
 english_keywords = [
     'event', 'festival', 'concert', 'party', 'celebration',
     'gathering', 'exhibition', 'launch', 'ceremony', 'workshop',
-    'conference', 'meetup', 'gala', 'opening', 'show'
-]
+    'strike']
 german_keywords = [
     'ereignis', 'festival', 'konzert', 'party', 'feier',
     'zusammenkunft', 'ausstellung', 'einführung', 'zeremonie', 'workshop',
-    'konferenz', 'treffen', 'gala', 'eröffnung', 'show', 'aktion'
+    'konferenz', 'treffen', 'gala', 'eröffnung', 'show', 'aktion', 'streik'
 ]
+
+english_keywords_negative = [
+    'today', 'we did', 'made', 'striked', 'yesterday', 'tomorrow']
+german_keywords_negative = [
+    'heute', 'haben wir', 'gemacht', 'gestreikt', 'gestern', 'morgen'
+]
+
 english_weekdays = [
     'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
 ]
@@ -23,15 +31,18 @@ german_months = ["januar", "februar", "märz", "april", "mai", "juni", "juli", "
 date_pattern = r'/\d{1,2}[.\-\/][ ]?\d{1,2}[.\-\/]/'
 
 
-def classic_classifier(post):
+def classic_classifier(post: PostTracker):
     text = post.caption()
-    contains = False
+    contains = 0
     for word in (
             english_keywords + german_keywords + english_weekdays + german_weekdays + german_months + english_months):
         if word.lower() in text.lower():
-            contains = True
+            contains += 1
+    for word in (english_keywords_negative + german_keywords_negative):
+        if word.lower() in text.lower():
+            contains -= 1
     match = re.search(date_pattern, text, re.IGNORECASE)
-    if (match is not None and match.group() != "") or contains:
+    if (match is not None and match.group() != "") or contains >= 1:
         return True
     else:
         return False
