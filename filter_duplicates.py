44c15c9ddb69b89fe23302bfec07e383389b1b9c
pasewalck <pasewalck@posteo.net>
2026-02-14 13:05:44 +0100

    Use location util for filtering duplicates.

--- filter_duplicates.py
+++ filter_duplicates.py
@@ -3,11 +3,11 @@ import os
 
 from tracker import use_tracker
 from util.config import LLM_OUTPUT_FILE_NAME
+from util.location import location
 from util.use_json import use_json
 
 
 def main():
-    date_owner_key_set = set()
     datetime_map = {}
 
     with (use_tracker() as sync_tracker):
@@ -17,30 +17,35 @@ def main():
         for post in targets_sorted:
             if post.interpreted:
                 with use_json(os.path.join(post.directory(), LLM_OUTPUT_FILE_NAME)) as json:
-                    for event_json in json["events"]:
-                        key_datetime = datetime.datetime.fromisoformat(event_json['start_datetime']).timestamp()
-                        key = f"{key_datetime}{post.account_details.userid}"
-                        if key in date_owner_key_set:
-                            print(f"Marking Duplicate (Found via Date and Owner Matching): {event_json['title']} {key}")
-                            event_json['duplicate'] = True
-                        else:
+                    if "events" in json:
+                        for event_json in json["events"]:
+                            key_datetime = datetime.datetime.fromisoformat(event_json['start_datetime']).timestamp()
+                            key = f"{key_datetime}{post.account_details.userid}"
+                            event_json['duplicate'] = False
+
                             if key_datetime not in datetime_map.keys():
                                 datetime_map[key_datetime] = []
                                 datetime_map[key_datetime].append(event_json)
                             else:
+                                is_duplicate = False
                                 for compare_event_json in datetime_map[key_datetime]:
-                                    words1 = compare_event_json['title'].lower().split()
-                                    words2 = event_json['title'].lower().split()
+                                    title1 = compare_event_json['title'].lower().split()
+                                    title2 = event_json['title'].lower().split()
+                                    location1 = location(event_json["location"]).lower().split()
+                                    location2 = location(event_json["location"]).lower().split()
 
-                                    equal_word_count = sum(1 for word in words1 if word in words2)
-                                    if equal_word_count > len(words2) / 2:
+                                    equal_word_count_title = sum(1 for word in title1 if word in title2)
+                                    equal_word_count_ort = sum(1 for word in location1 if word in location2)
+
+                                    if equal_word_count_title > len(title2) / 2 and equal_word_count_ort > len(
+                                            title1) / 2:
                                         print(
-                                            f"Marking Duplicate (Found via Date and Title Matching): {event_json['title']} {key}")
+                                            f"Marking Duplicate (Found via Date and Title and Ort Matching): {event_json['title']}  {key}")
                                         event_json['duplicate'] = True
-                                    else:
-                                        datetime_map[key_datetime].append(event_json)
-
-                            date_owner_key_set.add(key)
+                                        is_duplicate = True
+                                        break
+                                if not is_duplicate:
+                                    datetime_map[key_datetime].append(event_json)
 
 
 if __name__ == "__main__":
