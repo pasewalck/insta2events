b01460a2fc03bbb142770cd00bf8b5fda8e6de16
jana <jana.pasewalck@posteo.net>
2026-02-15 17:25:16 +0100

    Add functions to lookup (fix) locations for an event.

--- util/config.py
+++ util/config.py
@@ -12,9 +12,11 @@ SYNC_FILE_NAME = os.getenv("SYNC_FILE_NAME", "sync.pkl")
 LLM_OUTPUT_FILE_NAME = os.getenv("LLM_OUTPUT_FILE_NAME", "result_llm.json")
 LLM_PROMPT_OUTPUT_FILE_NAME = os.getenv("LLM_PROMPT_OUTPUT_FILE_NAME", "prompt_llm.txt")
 PROMPT_CLASSIFY_FILE = os.getenv("PROMPT_VALUE_FILE", "./prompts/de/prompt-value.txt")
+PROMPT_FIX_LOCATION_FILE = os.getenv("PROMPT_FIX_LOCATION_FILE", "./prompts/de/prompt-fix-location.txt")
 PROMPT_INTERPRETER_FILE = os.getenv("PROMPT_PARSE_FILE", "./prompts/de/prompt-parse.txt")
 LLM_PASS_IMAGES_DIRECTLY = os.getenv("LLM_PASS_IMAGES_DIRECTLY", "False") == "True"
 MODEL_CLASSIFIER = os.getenv("MODEL_CLASSIFIER", "gemma3:4b")
+MODEL_FIX_LOCATION = os.getenv("MODEL_FIX_LOCATION", "gemma3:4b")
 MODEL_INTERPRETER = os.getenv("MODEL_INTERPRETER", "llama3.2-vision")
 DOWNLOAD_PHOTOS = os.getenv("DOWNLOAD_PHOTOS", "True") == "True"
 INTERPRETER_CLASSIFIER_MODE = os.getenv("INTERPRETER_CLASSIFIER_MODE", "basic")
@@ -25,7 +27,7 @@ SCRAPE_HASHTAGS = os.getenv("SCRAPE_HASHTAGS", "")
 SAVE_PROMPT = os.getenv("SAVE_PROMPT", "False") == "True"
 RERUN_INTERPRETER = os.getenv("RERUN_INTERPRETER", "False") == "True"
 RERUN_CLASSIFIER = os.getenv("RERUN_CLASSIFIER", "False") == "True"
-SYNC_SINCE = os.getenv("SYNC_SINCE", datetime(2025, 12, 1).isoformat())
+SYNC_SINCE = os.getenv("SYNC_SINCE", datetime(2026, 1, 1).isoformat())
 USE_IMGINN_LINK = os.getenv("USE_IMGINN_LINK", "False") == "True"
 
 CALDAV_URL = os.getenv("CALDAV_URL")
